286e51090f22098185a2e25fd75becba71237973
HanzPM <hanzputra06@gmail.com>
2023-03-27 09:27:16 +0700

    feat: detail resto

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,13 @@ const router = createRouter({
       path: "/restos",
       name: "restos",
       component: () => import("../views/RestoView.vue"),
-    }
+    },
+
+    {
+      path: "/restos/:id",
+      name: "restos-show",
+      component: () => import("../views/RestoDetailView.vue"),
+    },
   ]
 })
 
