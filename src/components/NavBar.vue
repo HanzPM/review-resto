6af8e4a8f99d37d365b1c2797860afcf7130b29e
HanzPM <hanzputra06@gmail.com>
2023-03-31 09:27:30 +0700

    feat:(navbar) styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,6 +7,7 @@
     const dropdown = ref(false)
     const repository_auth = useAuthRepository();
     const router = useRouter()
+    
     const logout = () => {
         localStorage.removeItem('user');
         localStorage.removeItem('access_token');
@@ -25,15 +26,17 @@
 
 
 <template>    
-        <div class="relative flex justify-between w-full bg-[#353535] p-6 mb-8 items-center ">
+        <div class="relative flex justify-between w-full bg-[#111111] p-6 mb-8 items-center shadow-xl shadow-black">
             <div>
                 <h1 class="text-4xl text-white font-bold">Vasilio</h1>
             </div>
-            <div class="flex justify-around items-center">
-                <router-link to="/restos" class="ml-2 px-4 font-bold text-white">Home</router-link>
-                <router-link to="about" class="ml-2 px-4 mr-4 font-bold text-white">About</router-link>
+            <div class="flex justify-around items-center absolute left-[60%]">
+                <router-link to="/restos" class="ml-2 px-3 font-bold text-white border-b-2 border-spacing-x-[-1px] border-transparent hover:border-gray-600 hover:text-gray-300  hover:border-spacing-x-3 transition-all duration-[0.3s] overflow-hidden">Home</router-link>
+                <router-link to="about" class="ml-2 px-3 mr-6 font-bold text-white border-b-2 border-spacing-x-[-1px] border-transparent hover:border-gray-600 hover:text-gray-300  hover:border-spacing-x-3 transition-all duration-[0.3s]">About</router-link>
                 <div v-if="userData">
-                    <button v-if="dropdown" @click="dropdownHide" class="dropdown-toggle bg-[#111111] px-2 py-1 text-white font-semibold rounded-md">
+                    <input type="text" id="search" data-search class="mr-8 outline-none border-2 border-white p-2 rounded-xl bg-transparent text-white focus:border-gray-600 transition-all duration-[0.3s]" placeholder="Search...">
+                    <button><ion-icon name="search-outline" class="text-white absolute left-[72%] top-[13px]"></ion-icon></button>
+                    <button v-if="dropdown" @click="dropdownHide" class="dropdown-toggle self-end bg-[#353535] px-2 py-2 text-white font-semibold rounded-md">
                         Hi, {{ userData.name }}
                         <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-lg w-[15%] bg-[#474747] text-white ">
                             <li class="p-2 border-b-2 border-b-[#353535] hover:bg-[#303030] hover:rounded-t-lg">
@@ -43,11 +46,11 @@
                                 Logout
                             </li>
                             <li class="p-2 border-b-[#353535] hover:bg-[#303030] hover:rounded-b-lg">
-                                <router-link to="create-resto" >Create Your Resto</router-link>
+                                <router-link to="create" >Create Your Resto</router-link>
                             </li>
                         </ul>
                     </button>
-                    <button v-else @click="dropdownShow" class="dropdown-toggle bg-[#111111] px-2 py-2 text-white font-semibold rounded-md">
+                    <button v-else @click="dropdownShow" class="dropdown-toggle bg-[#353535] px-2 py-2 text-white font-semibold rounded-md">
                         Hi, {{ userData.name }}
                         <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-sm w-[15%] bg-white text-black">
                             <li class="p-2  border-b-blue-300">
