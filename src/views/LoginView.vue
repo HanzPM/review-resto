c15c83afd5b4465859adb0fee3549160b175cfe0
HanzPM <hanzputra06@gmail.com>
2023-03-31 13:41:02 +0700

    feat:(style) styling login and register

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -31,43 +31,113 @@ const onSubmit = async () => {
     isLoggingIn.value = false;
 };
 </script>
+<style>
+@import url('https://preview.redd.it/jkwvasp0pgz31.jpg?width=960&crop=smart&auto=webp&v=enabled&s=496fcbe3ebdc56368f2e13792db174264898b97a');
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins',sans-serif;
+}
+section{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+}
+.slebew{
+    position: relative;
+    width: 400px;
+    height: 450px;
+    background: transparent;
+    border: 2px solid rgba(255,255,255,0.5);
+    border-radius: 20px;
+    backdrop-filter: blur(5px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+}
+.wew{
+    font-size: x-large;
+    color: #fff;
+    text-align: center;
+}
+.inputbox{
+    position: relative;
+    margin: 30px 0;
+    width: 310px;
+    border-bottom: 2px solid #fff;
+}
+.inputbox label{
+    position: absolute;
+    top: 50%;
+    left: 5px;
+    transform: translateY(-50%);
+    color: #fff;
+    font-size: 1em;
+    pointer-events: none;
+    transition: .5s;
+}
+input:focus ~ label,
+input:valid ~ label{
+top: -5px;
+}
+.inputbox input {
+    width: 100%;
+    height: 50px;
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 1em;
+    padding:0 35px 0 5px;
+    color: #fff;
+}
+.inputbox ion-icon{
+    position: absolute;
+    right: 8px;
+    color: #fff;
+    font-size: 1.2em;
+    top: 20px;
+}
+.loginbut{
+    width: 100%;
+    height: 40px;
+    border-radius: 40px;
+    background: #fff;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-size: 1em;
+    font-weight: 600;
+}
+.register{
+    font-size: .9em;
+    color: #fff;
+    text-align: center;
+    margin: 25px 0 10px;
+}
+</style>
 <template>
-  <main class="grid grid-cols-12 gap-4 min-h-screen">
-    <section class="col-span-6 bg-[#353535] h-full shadow-xl">
-      <form
-        method="post"
-        :action="route.path"
-        class="p-40"
-        @submit.prevent="onSubmit"
-      >
-        <div class="mb-4">
-          <label for="email" class="block text-white font-bold mb-2">Email</label>
-          <input
-            v-model="credentials.email"
-            type="email"
-            required
-            placeholder="someone@email.com"
-            class="border p-2 w-full bg-gray-50 outline-none focus:ring-4 focus:ring-[#606060] rounded"
-          />
+  <section>
+        <div class="slebew">
+            <div class="form-value">
+                <form :action="route.path" method="post"  @submit.prevent="onSubmit">
+                    <h2 class="wew">LogIn</h2>
+                    <div class="inputbox">
+                        <input v-model="credentials.email" type="email" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                      <input v-model="credentials.password" type="password" required>
+                      <label for="">Password</label>
+                    </div>
+                    <button type="submit" class="w-[100%] h-[40px] rounded-[40px] bg-white hover:bg-gray-200 font-bold transition-colors duration-[0.3s]">Log in</button>
+                    <div class="register">
+                        <p>Don't have a account <RouterLink to="register" class="text-white hover:text-gray-300">Register</RouterLink></p>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="mb-4">
-          <label for="password" class="block text-white font-bold mb-2">Password</label>
-          <input
-            v-model="credentials.password"
-            type="password"
-            required
-            class="border p-2 w-full bg-gray-50 outline-none focus:ring-4 focus:ring-[#606060] rounded"
-          />
-        </div>
-        <button
-          type="submit"
-          class="bg-[#454545] text-white p-2 w-full block hover:bg-[#505050] rounded transition-colors duration-200 select-none"
-        >
-          Masuk 
-        </button>
-        <div class="p-8 absolute top-[365px] font-bold text-white ">Don't have an acoount ? <RouterLink  class="text-blue-300 hover:text-blue-600 cursor-pointer" to="register">Register Now</RouterLink></div>
-      </form>
     </section>
-  </main>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -25,49 +25,121 @@
     isLoading.value = false;
   }
 </script>
+<style>
+@import url('https://preview.redd.it/jkwvasp0pgz31.jpg?width=960&crop=smart&auto=webp&v=enabled&s=496fcbe3ebdc56368f2e13792db174264898b97a');
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins',sans-serif;
+}
+section{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+}
+.form-box{
+    position: relative;
+    width: 400px;
+    height: 550px;
+    background: transparent;
+    border: 2px solid rgba(255,255,255,0.5);
+    border-radius: 20px;
+    backdrop-filter: blur(5px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.wew{
+    font-size: x-large;
+    color: #fff;
+    text-align: center;
+}
+.inputbox{
+    position: relative;
+    margin: 30px 0;
+    width: 310px;
+    border-bottom: 2px solid #fff;
+}
+.inputbox label{
+    position: absolute;
+    top: 50%;
+    left: 5px;
+    transform: translateY(-50%);
+    color: #fff;
+    font-size: 1em;
+    pointer-events: none;
+    transition: .5s;
+}
+input:focus ~ label,
+input:valid ~ label{
+top: -5px;
+}
+.inputbox input {
+    width: 100%;
+    height: 50px;
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 1em;
+    padding:0 35px 0 5px;
+    color: #fff;
+}
+.inputbox ion-icon{
+    position: absolute;
+    right: 8px;
+    color: #fff;
+    font-size: 1.2em;
+    top: 20px;
+}
+.wow{
+    width: 100%;
+    height: 40px;
+    border-radius: 40px;
+    background: #fff;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-size: 1em;
+    font-weight: 600;
+}
+.register{
+    font-size: .9em;
+    text-align: center;
+    margin: 25px 0 10px;
+}
+</style>
 
 <template>
-  <Loading v-if="isLoading"/>
-  <main class="grid grid-cols-12 gap-4 overflow-hidden" v-else>
-    <section class="col-span-6 bg-[#353535] h-screen shadow-lg">
-        <form :action="route.path" method="post" class="w-[50%] mx-auto mt-20" @submit.prevent="onSubmit">
-            <div class="mb-4">
-              <label for="name" class="block text-white font-bold mb-2">Name</label>
-              <input 
-              type="text" 
-              class="border p-2 w-full bg-gray-50 rounded outline-none focus:ring-2 focus:ring-blue-300 transition-all"
-              placeholder="Someone"
-              v-model="credentials.name" 
-              required>
+  <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form :action="route.path" method="post"  @submit.prevent="onSubmit">
+                    <h2 class="wew">Register</h2>
+                    <div class="inputbox">
+                        <input v-model="credentials.name" type="string" required>
+                        <label for="">Username</label>
+                    </div>
+                    <div class="inputbox">
+                        <input v-model="credentials.email" type="email" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                      <input v-model="credentials.password" type="password" required>
+                      <label for="">Password</label>
+                    </div>
+                    <div class="inputbox">
+                        <input v-model="credentials.password_confirmation" type="password" required>
+                        <label for="">Confirm Pasword</label>
+                    </div>
+                    <button type="submit" class="w-[100%] h-[40px] rounded-[40px] bg-white hover:bg-gray-300 font-bold transition-colors duration-[0.3s]">Confirm</button>
+                    <div class="register">
+                        <p>Don't have a account <RouterLink to="/" class="text-white hover:text-gray-300">Log In</RouterLink></p>
+                    </div>
+                </form>
             </div>
-            <div class="mb-4">
-              <label for="email" class="block text-white font-bold mb-2">Email</label>
-              <input 
-              type="email" 
-              class="border p-2 w-full bg-gray-50 outline-none focus:ring-4 focus:ring-[#606060] rounded"
-              placeholder="someone@example.com"
-              v-model="credentials.email" 
-              required>
-            </div>
-            <div class="mb-4">
-              <label for="password" class="block text-white font-bold mb-2">Password</label>
-              <input 
-              type="password" 
-              class="border p-2 w-full bg-gray-50 outline-none focus:ring-4 focus:ring-[#606060] rounded"
-              v-model="credentials.password" 
-              required>
-            </div>
-            <div class="mb-4">
-              <label for="password_confirmation" class="block text-white font-bold mb-2">Password Confirmation</label>
-              <input 
-              type="password" 
-              class="border p-2 w-full bg-gray-50 outline-none focus:ring-4 focus:ring-[#606060] rounded"
-              v-model="credentials.password_confirmation" 
-              required>
-            </div>
-            <button type="submit" class="bg-[#454545] text-white p-2 w-full block hover:bg-[#505050] rounded transition-colors duration-200 select-none">Masuk</button>
-            <p class="text-center mt-5 font-bold text-white">Already Have an Account ? <router-link class="text-blue-300 hover:text-blue-600 cursor-pointer" to="/" >Login</router-link></p>
-          </form>
+        </div>
     </section>
-  </main>
 </template>
