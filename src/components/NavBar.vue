f1483c8a72972465bb4d0a6131867150f8557e70
HanzPM <hanzputra06@gmail.com>
2023-04-01 12:26:22 +0700

    fix:(navbar)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,30 +22,25 @@
     }
 </script>
 
-
-
-
 <template>    
-        <div class="relative flex justify-between w-full bg-[#111111] p-6 mb-8 items-center shadow-xl shadow-black">
+        <div class="relative flex justify-between w-full bg-[#111111] p-6 mb-8 items-center shadow-xl shadow-black select-none">
             <div>
                 <h1 class="text-4xl text-white font-bold">Vasilio</h1>
             </div>
-            <div class="flex justify-around items-center absolute left-[60%]">
-                <router-link to="/restos" class="ml-2 px-3 font-bold text-white border-b-2 border-spacing-x-[-1px] border-transparent hover:border-gray-600 hover:text-gray-300  hover:border-spacing-x-3 transition-all duration-[0.3s] overflow-hidden">Home</router-link>
-                <router-link to="about" class="ml-2 px-3 mr-6 font-bold text-white border-b-2 border-spacing-x-[-1px] border-transparent hover:border-gray-600 hover:text-gray-300  hover:border-spacing-x-3 transition-all duration-[0.3s]">About</router-link>
+            <div class="flex justify-around items-center">
+                <router-link to="/restos" class="ml-2    px-2 font-bold text-white border-b-2 border-spacing-x-[-1px] border-transparent hover:border-gray-600 hover:text-gray-300  hover:border-spacing-x-3 transition-all duration-[0.3s] overflow-hidden">Home</router-link>
+                <router-link to="" class="ml-2 px-2 mr-6 font-bold text-white border-b-2 border-spacing-x-[-1px] border-transparent hover:border-gray-600 hover:text-gray-300  hover:border-spacing-x-3 transition-all duration-[0.3s]">About</router-link>
                 <div v-if="userData">
-                    <input type="text" id="search" data-search class="mr-8 outline-none border-2 border-white p-2 rounded-xl bg-transparent text-white focus:border-gray-600 transition-all duration-[0.3s]" placeholder="Search...">
-                    <button><ion-icon name="search-outline" class="text-white absolute left-[72%] top-[13px]"></ion-icon></button>
                     <button v-if="dropdown" @click="dropdownHide" class="dropdown-toggle self-end bg-[#353535] px-2 py-2 text-white font-semibold rounded-md">
                         Hi, {{ userData.name }}
-                        <ul v-if="dropdown" class="dropdown-group absolute mt-2 right-5 rounded-lg w-[15%] bg-[#474747] text-white ">
-                            <li class="p-2 border-b-2 border-b-[#353535] hover:bg-[#303030] hover:rounded-t-lg">
+                        <ul v-if="dropdown" class="dropdown-group absolute z-10 mt-2 right-5 rounded-lg w-[15%] bg-[#6b6b6b] text-white ">
+                            <li class="p-2 border-b-2 border-b-[#5a5a5a] hover:bg-[#7777] hover:rounded-t-lg">
                                 <router-link to="profile">Profile</router-link>
                             </li>
-                            <li @click="logout" class="p-2 border-b-2 border-b-[#353535] hover:bg-[#303030]">
+                            <li @click="logout" class="p-2 border-b-2 border-b-[#5a5a5a] hover:bg-[#7777]">
                                 Logout
                             </li>
-                            <li class="p-2 border-b-[#353535] hover:bg-[#303030] hover:rounded-b-lg">
+                            <li class="p-2 border-b-[#10101010] hover:bg-[#7777] hover:rounded-b-lg">
                                 <router-link to="create" >Create Your Resto</router-link>
                             </li>
                         </ul>
